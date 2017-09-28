74c7d151d73a6f2a8cad26dea4b842cb8eab3714
Rodrigo Abreu <rodrigo.abreu.ca@gmail.com>
2017-09-28 01:58:07 -0700

    Creating the main UI sliding functionality in horizontal and vertical like in clash royale

---
+++ Assets/Scripts/RoyaleScroller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoyaleScroller : MonoBehaviour
+{
+    private float x, y;
+
+    public ScrollRect mainScrollRect;
+
+    public ScrollRect[] columnRects;
+
+    public bool isDragingHorizontally = false;
+
+    void Update()
+    {
+        if (!isDragingHorizontally)
+        {
+            x = Input.GetAxis("Mouse X");
+            y = Input.GetAxis("Mouse Y");
+
+            if (Mathf.Abs(x) > Mathf.Abs(y) + 1f)
+            {
+                isDragingHorizontally = true;
+                DisableScrollRects();
+            }
+            else
+            {
+                isDragingHorizontally = false;
+                EnableScrollRects();
+            }
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isDragingHorizontally = false;
+            EnableScrollRects();
+        }
+    }
+
+    private void DisableScrollRects()
+    {
+        HandleScrollRectsState(false);
+    }
+
+    private void EnableScrollRects()
+    {
+        HandleScrollRectsState(true);
+    }
+
+    private void HandleScrollRectsState(bool state)
+    {
+        if (columnRects != null)
+        {
+            for (int i = 0; i < columnRects.Length; i++)
+            {
+                columnRects[i].enabled = state;
+            }
+        }
+    }
+}
